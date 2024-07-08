b3fab3cc51e61def0a8dbd325cf0ef4247377fe4
bpy1401 <briand.patrick@free.fr>
2024-07-08 17:03:31 +0200

    Move XTEND unit test to JAVA unit tests (SWI, SWI2, SWI3)

---
+++ org.bpy.electronics.mc6809.assembler.tests/src/org/bpy/electronics/mc6809/assembler/tests/language/instructions/TestSWI3Instruction.java
@@ -0,0 +1,218 @@
+/*
+ * MC6809 Toolkit
+ * Copyright (C) 2023  Patrick BRIAND
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PASWI3CULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.bpy.electronics.mc6809.assembler.tests.language.instructions;
+
+import org.bpy.electronics.mc6809.assembler.assembler.AssemblerPackage;
+import org.bpy.electronics.mc6809.assembler.assembler.Swi3Instruction;
+import org.bpy.electronics.mc6809.assembler.assembler.InstructionLine;
+import org.bpy.electronics.mc6809.assembler.assembler.Model;
+import org.bpy.electronics.mc6809.assembler.assembler.SourceLine;
+import org.bpy.electronics.mc6809.assembler.engine.AssemblerEngine;
+import org.bpy.electronics.mc6809.assembler.engine.data.instructions.AssembledSWI3Instruction;
+import org.bpy.electronics.mc6809.assembler.tests.AssemblerInjectorProvider;
+import org.eclipse.xtext.testing.InjectWith;
+import org.eclipse.xtext.testing.XtextRunner;
+import org.eclipse.xtext.testing.util.ParseHelper;
+import org.eclipse.xtext.testing.validation.ValidationTestHelper;
+import org.eclipse.xtext.xbase.lib.Extension;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.google.inject.Inject;
+
+@RunWith(XtextRunner.class)
+@InjectWith(AssemblerInjectorProvider.class)
+
+public class TestSWI3Instruction {
+	@Inject ParseHelper<Model> parseHelper;
+	@Inject @Extension private ValidationTestHelper validationHelper;
+	
+	/**
+	 * Check SWI3 with extra space
+	 */
+	@Test 
+	public void testSimpleSWI3WithExtraSpace() {
+		StringBuilder strBuilder = new StringBuilder();
+		strBuilder.append("; -----------------------------------------\n");
+		strBuilder.append("	       ORG    		$8000\n");
+		strBuilder.append("	       SWI3  \n");
+		
+		try {
+			Model result = parseHelper.parse(strBuilder.toString());
+
+			Assert.assertNotNull(result);
+			Assert.assertTrue("No errors found", result.eResource().getErrors().isEmpty());
+			validationHelper.assertNoErrors(result);		
+
+			SourceLine line = result.getSourceLines().get(2);
+			Assert.assertTrue("Must be an Instruction line", line.getLineContent() instanceof InstructionLine);
+			
+			InstructionLine instructionLine = (InstructionLine)line.getLineContent();
+			Assert.assertTrue("Must be an SWI3 Accumulator line", instructionLine.getInstruction() instanceof Swi3Instruction);
+			Swi3Instruction swi3Instruction = (Swi3Instruction)instructionLine.getInstruction();
+			Assert.assertEquals("Must be an SWI3 instruction", "SWI3", swi3Instruction.getInstruction());
+		} catch (Exception e) {
+			Assert.assertTrue("Exception",true);
+		} 
+	}
+	
+	/**
+	 * Check SWI3 with extra space
+	 */
+	@Test 
+	public void testSimpleSWI3WithoutExtraSpace() {
+		StringBuilder strBuilder = new StringBuilder();
+		strBuilder.append("; -----------------------------------------\n");
+		strBuilder.append("	       ORG    $8000\n");
+		strBuilder.append("	       SWI3\n");
+		
+		try {
+			Model result = parseHelper.parse(strBuilder.toString());
+
+			Assert.assertNotNull(result);
+			Assert.assertTrue("No errors found", result.eResource().getErrors().isEmpty());
+			validationHelper.assertNoErrors(result);		
+		
+			SourceLine line = result.getSourceLines().get(2);
+			Assert.assertTrue("Must be an Instruction line", line.getLineContent() instanceof InstructionLine);
+			
+			InstructionLine instructionLine = (InstructionLine)line.getLineContent();
+			Assert.assertTrue("Must be an SWI3 Accumulator line", instructionLine.getInstruction() instanceof Swi3Instruction);
+			Swi3Instruction swi3Instruction = (Swi3Instruction)instructionLine.getInstruction();
+			Assert.assertEquals("Must be an SWI3 instruction", "SWI3", swi3Instruction.getInstruction());
+		} catch (Exception e) {
+			Assert.assertTrue("Exception",true);
+		} 
+	}
+	
+	/**
+	 * Check SWI3 with extra space
+	 */
+	@Test 
+	public void testSimpleSWI3WithExtraSpaceWithComment() {
+		StringBuilder strBuilder = new StringBuilder();
+		strBuilder.append("; -----------------------------------------\n");
+		strBuilder.append("	       ORG    	$8000\n");
+		strBuilder.append("	       SWI3  			; It is a comment \n");
+		
+		try {
+			Model result = parseHelper.parse(strBuilder.toString());
+
+			Assert.assertNotNull(result);
+			Assert.assertTrue("No errors found", result.eResource().getErrors().isEmpty());
+			validationHelper.assertNoErrors(result);		
+		
+			SourceLine line = result.getSourceLines().get(2);
+			Assert.assertTrue("Must be an Instruction line", line.getLineContent() instanceof InstructionLine);
+			
+			InstructionLine instructionLine = (InstructionLine)line.getLineContent();
+			Assert.assertTrue("Must be an SWI3 Accumulator line", instructionLine.getInstruction() instanceof Swi3Instruction);
+			Swi3Instruction swi3Instruction = (Swi3Instruction)instructionLine.getInstruction();
+			Assert.assertEquals("Must be an SWI3 instruction", "SWI3", swi3Instruction.getInstruction());
+		} catch (Exception e) {
+			Assert.assertTrue("Exception",true);
+		} 
+	}
+	
+	/**
+	 * Check SWI3 with extra space
+	 */
+	@Test 
+	public void testSimpleSWI3WithoutExtraSpaceWithComment() {
+		StringBuilder strBuilder = new StringBuilder();
+		strBuilder.append("; -----------------------------------------\n");
+		strBuilder.append("	       ORG    $8000\n");
+		strBuilder.append("	       SWI3					; It is a comment\n");
+		
+		try {
+			Model result = parseHelper.parse(strBuilder.toString());
+
+			Assert.assertNotNull(result);
+			Assert.assertTrue("No errors found", result.eResource().getErrors().isEmpty());
+			validationHelper.assertNoErrors(result);		
+		
+			SourceLine line = result.getSourceLines().get(2);
+			Assert.assertTrue("Must be an Instruction line", line.getLineContent() instanceof InstructionLine);
+		
+			InstructionLine instructionLine = (InstructionLine)line.getLineContent();
+			Assert.assertTrue("Must be an SWI3 Accumulator line", instructionLine.getInstruction() instanceof Swi3Instruction);
+			Swi3Instruction swi3Instruction = (Swi3Instruction)instructionLine.getInstruction();
+			Assert.assertEquals("Must be an SWI3 instruction", "SWI3", swi3Instruction.getInstruction());
+		} catch (Exception e) {
+			Assert.assertTrue("Exception",true);
+		} 
+	}	
+	
+	/**
+	 * Check SWI3 instruction with duplicate label 
+	 */
+	@Test 
+	public void testSWI3WithDuplicateLabel() {
+		StringBuilder strBuilder = new StringBuilder();
+		strBuilder.append("; -----------------------------------------\n");
+		strBuilder.append("		   	   ORG    			$8000\n");
+		strBuilder.append("Const	   	EQU          	5\n");
+		strBuilder.append("Start		   NOP\n");
+		strBuilder.append("			      NOP    \n");
+		strBuilder.append("Start      	SWI3		  	\n");
+		
+		try {
+			Model result = parseHelper.parse(strBuilder.toString());
+
+			Assert.assertNotNull(result);
+			Assert.assertTrue("No errors found", result.eResource().getErrors().isEmpty());
+			validationHelper.assertError(result, AssemblerPackage.eINSTANCE.getInstructionLine(),
+				AssemblerEngine.DUPLICATE_LABEL,"Label Start is already defined");
+		} catch (Exception e) {
+			Assert.assertTrue("Exception",true);
+		} 
+	}
+	
+	/**
+	 * Check SWI3 assembly instruction  
+	 */
+	@Test 
+	public void testSWI3Assembly() {
+		StringBuilder strBuilder = new StringBuilder();
+		strBuilder.append("; -----------------------------------------\n");
+		strBuilder.append("		   		ORG    			$8000\n");
+		strBuilder.append("Start      	SWI3		  		    ; 103F   SWI3\n");
+		
+		try {
+			Model result = parseHelper.parse(strBuilder.toString());
+
+			Assert.assertNotNull(result);
+			Assert.assertTrue("No errors found", result.eResource().getErrors().isEmpty());
+			validationHelper.assertNoErrors(result);		
+		
+			AssemblerEngine engine = AssemblerEngine.getInstance();
+			Assert.assertEquals("Check PC Counter after instruction", 0x8002, engine.getCurrentPcValue());
+			AssembledSWI3Instruction line = (AssembledSWI3Instruction)engine.getAssembledLine(2);
+			Assert.assertEquals("Check opcode length", 2, line.getOpcode().length);
+			Assert.assertEquals("Check opcode", 0x11, line.getOpcode()[0]);
+			Assert.assertEquals("Check opcode", 0x3F, line.getOpcode()[1]);
+			Assert.assertEquals("Check operand length", 0, line.getOperand().length);
+			Assert.assertEquals("Check label", "Start" , line.getLabel());
+			Assert.assertEquals("Check comment", "; 103F   SWI3" , line.getComment());
+		} catch (Exception e) {
+			Assert.assertTrue("Exception",true);
+		} 
+	}
+}
