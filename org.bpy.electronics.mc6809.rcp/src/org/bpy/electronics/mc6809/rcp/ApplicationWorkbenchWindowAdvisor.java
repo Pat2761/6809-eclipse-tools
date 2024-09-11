3eb62d44878a63b4b4e4da2c9a565d8ae52d5937
bpy1401 <briand.patrick@free.fr>
2024-09-11 15:35:23 +0200

    Remove unexpected menu search
    cover:  [Platform] - Duplicate menu search #31

--- org.bpy.electronics.mc6809.rcp/src/org/bpy/electronics/mc6809/rcp/ApplicationWorkbenchWindowAdvisor.java
+++ org.bpy.electronics.mc6809.rcp/src/org/bpy/electronics/mc6809/rcp/ApplicationWorkbenchWindowAdvisor.java
@@ -1,10 +1,14 @@
 package org.bpy.electronics.mc6809.rcp;
 
+import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+import org.eclipse.ui.internal.WorkbenchWindow;
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
@@ -26,4 +30,21 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 		configurer.setShowStatusLine(true);
 		configurer.setTitle("6809 Toolkit");
 	}
+
+	@Override
+	public void postWindowOpen() {
+	    IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+	    IContributionItem[] items = ((WorkbenchWindow) workbenchWindow).getMenuBarManager().getItems();
+	    for (IContributionItem item : items) {
+
+	      switch (item.getId()) {
+	        case "org.eclipse.search.menu": //$NON-NLS-1$
+	          item.setVisible(false);
+	          break;
+	        default:  
+	      }
+	    }
+	}
+	
+	
 }
